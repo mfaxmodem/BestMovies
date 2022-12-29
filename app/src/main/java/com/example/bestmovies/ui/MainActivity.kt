077753e357f14d6d1ac850a73bf7e9b5893bc8b5
AmirAli <naserzadeh34@gmail.com>
2022-12-29 19:12:30 +0330

    Initial commit

---
+++ app/src/main/java/com/example/bestmovies/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bestmovies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bestmovies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
